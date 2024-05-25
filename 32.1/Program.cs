d70801bc67da36cd68fe2ba38ab56634e4b1d952
Clawiatura <maksimov19992805@icloud.com>
2024-05-25 14:33:54 +0200

    Добавьте файлы проекта.

---
+++ 32.1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+
+    string defaultPath = "D:\\Battle.net\\Hearthstone\\Data";
+    PrintDirectory(new DirectoryInfo(defaultPath), "", true);
+
+
+
+void PrintDirectory (DirectoryInfo directory, string indent, bool lastDirectory)
+{
+    Console.Write(indent);
+    if (lastDirectory)
+    {
+        Console.Write("└-");
+        indent += " ";
+    }
+    else
+    {
+        Console.Write("├");
+        indent += "| ";
+    }
+    Console.WriteLine(directory.Name);
+
+    DirectoryInfo[] subDirectories = directory.GetDirectories();
+    for (int i = 0; i < subDirectories.Length; i++)
+    {
+        PrintDirectory(subDirectories[i], indent, i == subDirectories.Length - 1);
+    }
+}
